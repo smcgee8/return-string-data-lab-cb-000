aac081f19d1677211efe4082b1296904c4f7d542
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-11 18:43:22 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
 
   def inventory
     product = Product.find(params[:id])
-    truthy = !!(product.inventory > 0)
+    truthy = !!(product.inventory > 0) || false
     render plain: truthy
   end
 
