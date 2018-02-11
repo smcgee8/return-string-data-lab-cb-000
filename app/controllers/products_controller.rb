bbdd9b8965caf3590e0703437460722a9bace28c
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-11 18:15:39 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to product_path(@product)
+      redirect_to products_path
     else
       render :new
     end
